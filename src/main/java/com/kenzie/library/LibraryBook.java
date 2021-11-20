3d17cc3b2227c52b4148df6f0686dfa5d6e230e2
Behzod Mamadiev <bmamadiev@gmail.com>
2021-11-20 16:58:49 -0500

    Design Package Assessment

--- src/main/java/com/kenzie/library/LibraryBook.java
+++ src/main/java/com/kenzie/library/LibraryBook.java
@@ -1,5 +1,66 @@
 package com.kenzie.library;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class LibraryBook{
+public class LibraryBook implements Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private ArrayList<String> subjectList;
 
+    public LibraryBook() {
+    }
+
+    public LibraryBook(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public void setBookInfo(String detailString) {
+        String[] bookArray = detailString.split("\\|");
+
+        if (bookArray.length == 1) {
+            this.title = bookArray[0];
+        } else if (bookArray.length == 2) {
+            this.title = bookArray[0];
+            this.author = bookArray[1];
+        } else if (bookArray.length == 3) {
+            this.title = bookArray[0];
+            this.author = bookArray[1];
+            this.isbn = bookArray[2];
+        } else if (bookArray.length == 4) {
+            this.title = bookArray[0];
+            this.author = bookArray[1];
+            this.isbn = bookArray[2];
+            bookArray[3] = bookArray[3].replace("[", "").replace("]", "");
+            this.subjectList = new ArrayList<>(Arrays.asList(bookArray[3].split(",")));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryBook{" +
+                "title='" + getTitle() + '\'' +
+                ", author='" + getAuthor() + '\'' +
+                ", isbn='" + getIsbn() + '\'' +
+                ", subjectList=" + getSubjectList() +
+                '}';
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public ArrayList<String> getSubjectList() {
+        return this.subjectList;
+    }
 }
--- src/main/java/com/kenzie/library/Main.java
+++ src/main/java/com/kenzie/library/Main.java
@@ -1,8 +1,38 @@
 package com.kenzie.library;
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    //Write a public static method returnBookList() in Main.java
+    //      a. the method takes in 5 formatted Strings
+    //      b. within the method, instantiate the LibraryBook objects
+    //      c. Add the LibraryBook objects to an ArrayList<LibraryBook>
+    //      d. return the Arraylist<LibraryBook>
+    public static ArrayList<LibraryBook> returnBookList(String book1, String book2, String book3, String book4, String book5) {
+        ArrayList<LibraryBook> list = new ArrayList<>();
+
+        LibraryBook bookOne = new LibraryBook();
+        bookOne.setBookInfo(book1);
+        list.add(bookOne);
+
+        LibraryBook bookTwo = new LibraryBook();
+        bookTwo.setBookInfo(book2);
+        list.add(bookTwo);
+
+        LibraryBook bookThree = new LibraryBook();
+        bookThree.setBookInfo(book3);
+        list.add(bookThree);
+
+        LibraryBook bookFour = new LibraryBook();
+        bookFour.setBookInfo(book4);
+        list.add(bookFour);
+
+        LibraryBook bookFive = new LibraryBook();
+        bookFive.setBookInfo(book5);
+        list.add(bookFive);
+
+        return list;
+    }
 
     public static void main(String[] args) {
         final String BOOK_1 = "The Hobbit|J.R.R. Tolkien||";
@@ -18,16 +48,26 @@ public class Main {
             //      a. Call BookRequest.sendGET()
             //      b. Get the String response as the return value
             //      c. Use Logger to log each response string
-            //3. Write a public static method returnBookList() in Main.java
-            //      a. the method takes in 5 formatted Strings
-            //      b. within the method, instantiate the LibraryBook objects
-            //      c. Add the LibraryBook objects to an ArrayList<LibraryBook>
-            //      d. return the Arraylist<LibraryBook>
+
+            Logger log = Logger.getInstance();
+
+            String responseOne = BookRequest.sendGET(BOOK_1);
+            log.logMessage(responseOne);
+            String responseTwo = BookRequest.sendGET(BOOK_2);
+            log.logMessage(responseTwo);
+            String responseThree = BookRequest.sendGET(BOOK_3);
+            log.logMessage(responseThree);
+            String responseFour = BookRequest.sendGET(BOOK_4);
+            log.logMessage(responseFour);
+            String responseFive = BookRequest.sendGET(BOOK_5);
+            log.logMessage(responseFive);
+
             //4. Call the returnBookList() and print the results with System.out.println()
             //        to make sure the method works and toString() is overridden.
+            System.out.println(returnBookList(BOOK_1, BOOK_2, BOOK_3, BOOK_4, BOOK_5));
             //5. User Logger to log: "Process complete"
+            log.logMessage("Process complete");
             //6. Unit Test your code with LibraryBookTest.java and MainTest.java
-
     }
         catch (Exception e){
             System.out.println("Error occurred: unable to request book info");
